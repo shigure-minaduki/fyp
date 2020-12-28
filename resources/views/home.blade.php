86b40787295658eafc4477c697d0089d8dc45b9e
shigure-minaduki <tungsenchi@gmail.com>
2020-12-28 08:40:42 +0000

    heroku set up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    HKBU
+                    HKBU Online Exam System
                 </div>
 
                 <div class="card-body">
