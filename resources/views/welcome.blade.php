9ec3b3f18c63082a3d69a16116e129612947bfb5
shigure-minaduki <tungsenchi@gmail.com>
2021-03-29 06:43:43 +0000

    final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 
         <title>HKBU</title>
 
-        <!-- Fonts -->
+        <!-- Fonts 1 -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
